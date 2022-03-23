b66941d5091a79411ef36b5f50cdcf0bc53bb1d0
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2022-03-23 19:07:06 +0700

    Add closing menu on wallet creation

--- actions/createWallet.ts
+++ actions/createWallet.ts
@@ -2,10 +2,12 @@ import {Context} from "grammy";
 import activeMenu from "../menus/activeMenu";
 import {Wallet} from 'ethers';
 import {randomBytes} from "crypto";
+import {MenuFlavor} from "@grammyjs/menu/out/menu";
 
-const createWallet = (ctx: Context) => {
+const createWallet = (ctx: Context & MenuFlavor) => {
     const privateKey = "0x" + randomBytes(32).toString('hex');
     const wallet = new Wallet(privateKey);
+    ctx.menu.close();
     return ctx.reply(`Кошелёк создан, адрес ${wallet.address}`, {reply_markup: activeMenu});
 };
 
