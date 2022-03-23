14a26aca00fc9aed5c103ce6c9bb6f5d4371ba63
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2022-03-23 17:02:08 +0700

    Make bot reply with hello to any text

--- app.ts
+++ app.ts
@@ -1,33 +1,17 @@
 // Based on https://github.com/telegraf/telegraf/blob/v4/docs/examples/express-webhook-bot.ts
 
-import express, { Request, Response } from 'express'
-import { Telegraf } from 'telegraf'
+import { Telegraf } from 'telegraf';
+import {config} from 'dotenv';
+
+config({path: '.env.local'});
 
 const BOT_TOKEN = process.env.BOT_TOKEN
-if (BOT_TOKEN === undefined) {
-    throw new Error('BOT_TOKEN must be provided!')
-}
 
-const BOT_DOMAIN = process.env.BOT_DOMAIN
-if (BOT_DOMAIN === undefined) {
-    throw new Error('BOT_DOMAIN must be provided!')
+if (BOT_TOKEN === undefined) {
+    throw new Error('BOT_TOKEN must be provided')
 }
 
 const bot = new Telegraf(BOT_TOKEN)
 // Set the bot response
 bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
-
-const secretPath = `/telegraf/${bot.secretPathComponent()}`
-
-// Set telegram webhook
-bot.telegram.setWebhook(`${BOT_DOMAIN}${secretPath}`)
-
-const app = express()
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
-// Set the bot API endpoint
-app.use(bot.webhookCallback(secretPath))
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
-})
-
-// No need to call bot.launch()
+bot.launch()
