e0332c78b53864cc753673b2e292c8780ded3def
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2022-03-23 17:45:49 +0700

    Rewrite to grammy and add commands list

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 // Based on https://github.com/telegraf/telegraf/blob/v4/docs/examples/express-webhook-bot.ts
 
-import { Telegraf } from 'telegraf';
 import {config} from 'dotenv';
+import { Bot } from "grammy";
 
 config({path: '.env.local'});
 
@@ -11,7 +11,19 @@ if (BOT_TOKEN === undefined) {
     throw new Error('BOT_TOKEN must be provided')
 }
 
-const bot = new Telegraf(BOT_TOKEN)
-// Set the bot response
-bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
-bot.launch()
+const bot = new Bot(BOT_TOKEN);
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", async (ctx) => {
+    await bot.api.setMyCommands([
+        { command: "create", description: "Создание кошелька" },
+        { command: "export", description: "Экспорт кошелька" },
+        { command: "import", description: "Импорт кошелька" },
+        { command: "deposit", description: "Пополнение кошелька" },
+        { command: "balance", description: "Просмотр баланса" },
+        { command: "send", description: "Отправка средств" },
+        { command: "address", description: "Узнать адрес своего кошелька" },
+    ]);
+    return ctx.reply("Got another message!");
+});
+bot.start();
+
