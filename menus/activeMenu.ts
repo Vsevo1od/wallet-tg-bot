d0918ab2e4ecd1f0cb0bc2ff70e6cac33c244c3e
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2022-04-21 15:46:29 +0700

    Add closing menu

--- menus/activeMenu.ts
+++ menus/activeMenu.ts
@@ -4,15 +4,18 @@ import {Wallet} from "ethers";
 import {assertIsPrivateKey} from "../validation/isPrivateKey";
 import provider from "../modules/provider";
 import {formatEther} from "ethers/lib/utils";
+import {MenuFlavor} from "@grammyjs/menu/out/menu";
 
 let activeMenu: Menu<MyContext>;
 
-const exportWallet = async (ctx: MyContext) => {
+const exportWallet = async (ctx: MyContext & MenuFlavor) => {
+    ctx.menu.close();
     assertIsPrivateKey(ctx.session.privateKey);
     await ctx.reply(ctx.session.privateKey);
     return ctx.reply("Экспорт кошелька", {reply_markup: activeMenu});
 };
-const deposit = (ctx: MyContext) => {
+const deposit = (ctx: MyContext & MenuFlavor) => {
+    ctx.menu.close();
     assertIsPrivateKey(ctx.session.privateKey);
     const wallet = new Wallet(ctx.session.privateKey)
     return ctx.reply(
@@ -20,13 +23,15 @@ const deposit = (ctx: MyContext) => {
         {reply_markup: activeMenu, parse_mode: "HTML" }
     );
 };
-const getBalance = async (ctx: MyContext) => {
+const getBalance = async (ctx: MyContext & MenuFlavor) => {
+    ctx.menu.close();
     assertIsPrivateKey(ctx.session.privateKey);
     const wallet = new Wallet(ctx.session.privateKey, provider);
     const balance = formatEther(await wallet.getBalance());
     return ctx.reply(`Ваш баланс: ${balance} eth`, {reply_markup: activeMenu});
 };
-const getAddress = async (ctx: MyContext) => {
+const getAddress = async (ctx: MyContext & MenuFlavor) => {
+    ctx.menu.close();
     assertIsPrivateKey(ctx.session.privateKey);
     const wallet = new Wallet(ctx.session.privateKey);
     return ctx.reply(`Ваш адрес: ${wallet.address}`, {reply_markup: activeMenu});
