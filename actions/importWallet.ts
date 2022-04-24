a7f81e163d79d82700cb8f3bdae2a94275230ae2
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2022-04-24 12:30:22 +0700

    Move create and import from actions to menu

--- actions/importWallet.ts
+++
@@ -1,7 +0,0 @@
-import {MyContext} from "../types/MyContext";
-import {State} from "../types/State";
-
-export default (ctx: MyContext) => {
-    ctx.session.state = State.Importing;
-    return ctx.reply("Пожалуйста, отправьте приватный ключ в ответном сообщении");
-};
--- menus/newMenu.ts
+++ menus/newMenu.ts
@@ -1,7 +1,22 @@
 import {Menu} from "@grammyjs/menu";
 import {MyContext} from "../types/MyContext";
-import createWallet from "../actions/createWallet";
-import importWallet from "../actions/importWallet";
+import {MenuFlavor} from "@grammyjs/menu/out/menu";
+import {randomBytes} from "crypto";
+import {Wallet} from "ethers";
+import activeMenu from "./activeMenu";
+import {State} from "../types/State";
+
+const createWallet = (ctx: MyContext & MenuFlavor) => {
+    ctx.session.privateKey = "0x" + randomBytes(32).toString('hex');
+    const wallet = new Wallet(ctx.session.privateKey);
+    ctx.menu.close();
+    return ctx.reply(`Кошелёк создан, адрес ${wallet.address}`, {reply_markup: activeMenu});
+};
+
+const importWallet = (ctx: MyContext) => {
+    ctx.session.state = State.Importing;
+    return ctx.reply("Пожалуйста, отправьте приватный ключ в ответном сообщении");
+}
 
 export default new Menu<MyContext>("new-menu")
     .text("Создание кошелька", createWallet)
