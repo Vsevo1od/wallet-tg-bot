cd75a6d00e729570a44201e58032ca02270758e5
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2022-05-07 14:40:35 +0700

    Add tests for export and import

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,6 +3,9 @@ import bot from "../bot";
 import {Methods, Payload, RawApi} from "grammy/out/core/client";
 import {InlineKeyboardButton, Update} from "grammy/out/platform.node";
 
+const TEST_PRIVATE_KEY = '0xcb45a73faa747644a75b69f9a2941eb246f21cf3517d8aad1839afa69d330ca6';
+const TEST_WALLET_ADDRESS = '0xdD549BeB5dedED9720389F8F99c2ef28514b4739';
+
 type OutgoingRequest = {
     method: Methods<RawApi>,
     payload:  Payload<Methods<RawApi>, RawApi>
@@ -99,6 +102,18 @@ function generateMessageWithCallback(data: string): Update {
  }
 }
 
+function getCallbackData(callbackDataText: string, request?: OutgoingRequest): string {
+    const data = request
+        ?.payload
+        ?.reply_markup
+        ?.inline_keyboard[0]
+        .find(({text}) => text === callbackDataText)
+        ?.callback_data as string
+    ;
+    expect(typeof data).toBe('string');
+    return data;
+}
+
 beforeAll(async () => {
     bot.api.config.use((prev, method, payload: any, signal) => {
         outgoingRequests.push({ method, payload, signal });
@@ -129,15 +144,37 @@ test("start", async () => {
 
 test("create wallet", async () => {
     await bot.handleUpdate(generateMessage("/start"));
-    const createCommandCallback: string = outgoingRequests[0]
-        ?.payload
-        ?.reply_markup
-        ?.inline_keyboard[0]
-        .find(({text}) => text === 'Создание кошелька')
-        ?.callback_data as string
-    ;
-    expect(typeof createCommandCallback).toBe('string');
-    await bot.handleUpdate(generateMessageWithCallback(createCommandCallback as string));
+    const createCommandCallbackData = getCallbackData('Создание кошелька', outgoingRequests[0]);
+    await bot.handleUpdate(generateMessageWithCallback(createCommandCallbackData));
     expect(outgoingRequests.length).toBe(4);
     expect(outgoingRequests[2]?.payload.text).toMatch(/Кошелёк создан, адрес 0x[A-Za-z0-9]{40}/);
+});
+
+test("import", async () => {
+    await bot.handleUpdate(generateMessage("/start"));
+
+    const importCommandCallbackData = getCallbackData('Импорт кошелька', outgoingRequests[0]);
+    await bot.handleUpdate(generateMessageWithCallback(importCommandCallbackData));
+    expect(outgoingRequests.length).toBe(3);
+    expect(outgoingRequests[1]?.payload.text).toBe('Пожалуйста, отправьте приватный ключ в ответном сообщении');
+
+    await bot.handleUpdate(generateMessage(TEST_PRIVATE_KEY));
+    expect(outgoingRequests.length).toBe(4);
+    expect(outgoingRequests[3]?.payload.text).toBe('Ключ импортирован');
+});
+
+test("export", async () => {
+    await bot.handleUpdate(generateMessage("/start"));
+
+    const importCommandCallbackData = getCallbackData('Импорт кошелька', outgoingRequests[0]);
+    await bot.handleUpdate(generateMessageWithCallback(importCommandCallbackData));
+
+    await bot.handleUpdate(generateMessage(TEST_PRIVATE_KEY));
+
+    const exportCommandCallbackData = getCallbackData('Экспорт кошелька', outgoingRequests[3]);
+    await bot.handleUpdate(generateMessageWithCallback(exportCommandCallbackData));
+
+    expect(outgoingRequests.length).toBe(8);
+    expect(outgoingRequests[4]?.payload.text).toBe(TEST_PRIVATE_KEY);
+    expect(outgoingRequests[6]?.payload.text).toBe('Экспорт кошелька');
 });
